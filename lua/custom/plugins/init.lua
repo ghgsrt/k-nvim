1038721e63720d629c24d7abe23443d3aa5593f6
ghgsrt <ajbosco00@gmail.com>
2025-02-25 20:33:31 -0500

    stuff

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,14 +2,36 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {{
-	'sainnhe/gruvbox-material',
-	lazy = false,
-	priority = 1000,
-	config = function()
-	  -- Optionally configure and load the colorscheme
-	  -- directly inside the plugin declaration.
-	  vim.g.gruvbox_material_enable_italic = true
-	  vim.cmd.colorscheme('gruvbox-material')
-	end
-  }}
+return {
+  'aymericbeaumet/vim-symlink',
+  'theprimeagen/harpoon',
+  'tpope/vim-fugitive',
+  'geogitorg/neogit',
+  {
+    'NeogitOrg/neogit',
+    dependencies = {
+      'nvim-lua/plenary.nvim', -- required
+      --"sindrets/diffview.nvim",        -- optional - Diff integration
+
+      'nvim-telescope/telescope.nvim', -- optional
+    },
+    config = true,
+  },
+  'nvim-treesitter/nvim-treesitter-context',
+  {
+    'sainnhe/gruvbox-material',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- Optionally configure and load the colorscheme
+      -- directly inside the plugin declaration.
+      vim.g.gruvbox_material_enable_italic = true
+      vim.cmd.colorscheme 'gruvbox-material'
+    end,
+  },
+  {
+    'HiPhish/guile.vim',
+    lazy = false,
+    priority = 500,
+  },
+}
